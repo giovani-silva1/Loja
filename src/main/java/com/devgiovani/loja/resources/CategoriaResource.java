6096fb4231171f0678a2527a98a80d2113915c82
Giovani Silva <giovani-silva1@live.com>
2021-03-19 17:17:42 -0300

    Criado a primeira classe Dominio - Categoria

--- src/main/java/com/devgiovani/loja/resources/CategoriaResource.java
+++ src/main/java/com/devgiovani/loja/resources/CategoriaResource.java
@@ -1,16 +1,27 @@
 package com.devgiovani.loja.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.devgiovani.loja.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String mecasei() {
-		return "Rest Funcionando"; 
+	public List<Categoria> listCategoria() {
+		Categoria categoria1 = new Categoria(1, "Informática");
+		Categoria categoria2 = new Categoria(2, "Escritório");
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(categoria1);
+		lista.add(categoria2);
+		
+		return lista;
 	}
 	
 	
